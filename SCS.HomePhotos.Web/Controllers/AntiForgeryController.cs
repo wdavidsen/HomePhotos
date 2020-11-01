bb24121d48719b45611699e8b614db2a45bf4e01
Bill Davidsen <Bill@Xensan.com>
2020-10-31 21:06:03 -0400

    Updated csref controller

---
+++ SCS.HomePhotos.Web/Controllers/AntiForgeryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SCS.HomePhotos.Web.Controllers
+{
+    // from: https://www.blinkingcaret.com/2018/11/29/asp-net-core-web-api-antiforgery/
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AntiForgeryController : Controller
+    {
+        private IAntiforgery _antiForgery;
+
+        public AntiForgeryController(IAntiforgery antiForgery)
+        {
+            _antiForgery = antiForgery;
+        }
+
+        [IgnoreAntiforgeryToken]
+        public IActionResult Get()
+        {
+            var tokens = _antiForgery.GetAndStoreTokens(HttpContext);
+            Response.Cookies.Append("XSRF-REQUEST-TOKEN", tokens.RequestToken, new Microsoft.AspNetCore.Http.CookieOptions
+            {
+                HttpOnly = false
+            });
+            return NoContent();
+        }
+    }
+}
+
