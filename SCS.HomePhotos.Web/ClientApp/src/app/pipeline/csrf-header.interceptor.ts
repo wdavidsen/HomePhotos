f4a1205cfffd55d02ab02b80aecd5a02c4986cd0
Bill Davidsen <Bill@Xensan.com>
2020-10-31 21:05:35 -0400

    Added csrf controller and intercepter

---
+++ SCS.HomePhotos.Web/ClientApp/src/app/pipeline/csrf-header.interceptor.ts
@@ -0,0 +1,26 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class CsrfHeaderInterceptor implements HttpInterceptor {
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const requestToken = this.getCookieValue('XSRF-REQUEST-TOKEN');
+
+        return next.handle(req.clone({
+            headers: req.headers.set('X-XSRF-TOKEN', requestToken)
+        }));
+    }
+
+    private getCookieValue(cookieName: string) {
+        const allCookies = decodeURIComponent(document.cookie).split('; ');
+
+        for (let i = 0; i < allCookies.length; i++) {
+            const cookie = allCookies[i];
+            if (cookie.startsWith(cookieName + '=')){
+                return cookie.substring(cookieName.length + 1);
+            }
+        }
+        return '';
+    }
+}
