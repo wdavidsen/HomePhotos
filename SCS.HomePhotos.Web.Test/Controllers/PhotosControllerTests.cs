6b93987ade16d8ab21ad25839be4826e8e573694
Bill Davidsen <Bill@Xensan.com>
2022-08-10 15:13:35 -0400

    Updated test packages

--- SCS.HomePhotos.Web.Test/Controllers/PhotosControllerTests.cs
+++ SCS.HomePhotos.Web.Test/Controllers/PhotosControllerTests.cs
@@ -31,8 +31,8 @@ namespace SCS.HomePhotos.Web.Test.Controllers
             _photosService = new Mock<IPhotoService>();
             _staticConfig = new Mock<IStaticConfig>();
 
-            _staticConfig.SetupGet(p => p.ImageEncryptKey).Returns("somekey");
-            _staticConfig.SetupGet(p => p.ImageEncryptPasscode).Returns("somepasscode");
+            _staticConfig.SetupGet(p => p.ImageEncryptKey).Returns("04869030587693020458265184096903");
+            _staticConfig.SetupGet(p => p.ImageEncryptPasscode).Returns("2958721564856927");
 
             _photosController = new PhotosController(_logger.Object, _photosService.Object, _staticConfig.Object);
         }
