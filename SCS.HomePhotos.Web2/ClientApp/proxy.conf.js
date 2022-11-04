4b8358e8f9ee202ff8d18f145fa8d85b1049ad98
Bill Davidsen <Bill@Xensan.com>
2022-11-04 15:18:59 -0400

    Fixed signalr and avatar endpoints

--- SCS.HomePhotos.Web2/ClientApp/proxy.conf.js
+++ SCS.HomePhotos.Web2/ClientApp/proxy.conf.js
@@ -6,7 +6,8 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 const PROXY_CONFIG = [
   {
     context: [
-      "/weatherforecast",
+      "/avatar-image",
+      "/message-hub"
    ],
     target: target,
     secure: false,
